22c55f137d88378c1de3d0d90742b67fbb848107
FurkanTheNazim <moussafurkan0@gmail.com>
2025-07-31 10:58:49 +0300

    have some problems about planing how to build this, i'll restart but i've come this far!

--- get_next_line.c
+++ get_next_line.c
@@ -5,8 +5,42 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mahmmous <mahmmous@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/27 23:08:16 by mahmmous          #+#    #+#             */
-/*   Updated: 2025/07/27 23:08:16 by mahmmous         ###   ########.fr       */
+/*   Created: 2025/07/28 04:07:32 by mahmmous          #+#    #+#             */
+/*   Updated: 2025/07/28 04:07:32 by mahmmous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+	static char *stash;
+	char buffer[BUFFER_SIZE + 1];
+	int bytes_read;
+	int i;
+	char *temp;
+
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	bytes_read = 1;
+	while ((!stash || !ft_strchr(stash, '\n')) && bytes_read != 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+
+		if (bytes_read == -1)
+		{
+			free(stash);
+			return (NULL);
+		}
+		if (bytes_read == 0)
+			break;
+		buffer[bytes_read] = '\0';
+		temp = stash;
+		stash = ft_strjoin(temp, buffer);
+		free(temp);
+	}
+	
+	
+
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -10,6 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t  ft_strlen(const char *s)
+{
+        size_t  len;
+
+        len = 0;
+        while (s[len])
+                len++;
+        return (len);
+}
+
+char    *ft_strdup(const char *s1)
+{
+        char    *dup;
+        size_t  len;
+        size_t  i;
+
+        len = ft_strlen(s1);
+        dup = (char *)malloc(sizeof(char) * (len + 1));
+        if (!dup)
+                return (NULL);
+        i = 0;
+        while (i < len)
+        {
+                dup[i] = s1[i];
+                i++;
+        }
+        dup[i] = '\0';
+        return (dup);
+}
+
 char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
@@ -23,15 +55,15 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*str;
 	size_t	len1;
 	size_t	len2;
 	size_t	i;
 
-	if (!s1 || !s2)
-		return (NULL);
+	if (!s2)
+		return (s1);
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
 	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
@@ -43,6 +75,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		str[i] = s1[i];
 		i++;
 	}
+    free(s1);
 	while (i < len1 + len2)
 	{
 		str[i] = s2[i - len1];
