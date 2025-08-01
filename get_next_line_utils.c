f371c1fee6f53f1fadf0c5bb784de96722888a81
FurkanTheNazim <moussafurkan0@gmail.com>
2025-08-01 15:39:18 +0300

    every funciton fixed acording to subject and norminette, but TIMEOUT problem still standing

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -63,7 +63,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	i;
 	size_t	j;
 
-	i = 0;
 	if (!s1 && !s2)
 		return (NULL);
 	if (!s1)
@@ -73,19 +72,14 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
 		return (NULL);
-	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
+	i = 0;
 	j = 0;
-	while (s2[j])
-	{
-		str[i] = s2[j];
-		i++;
-		j++;
-	}
-	str[i] = '\0';
+	while (s1[i])
+		str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		str[j++] = s2[i++];
+	str[j] = '\0';
 	return (str);
 }
 
